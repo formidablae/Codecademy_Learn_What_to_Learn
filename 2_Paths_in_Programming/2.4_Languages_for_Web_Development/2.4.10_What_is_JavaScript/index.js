1710e622ea544950c265fbd656862d5f23ce80ad
formidablae <81068781+formidablae@users.noreply.github.com>
2022-09-29 00:17:42 +0200

    Did 2 Paths in Programming / 2.4 Languages for Web Development

---
+++ 2_Paths_in_Programming/2.4_Languages_for_Web_Development/2.4.10_What_is_JavaScript/index.js
@@ -0,0 +1,5 @@
+// Change the text between the quotation marks and run your code to see the name of your favorite language animated!
+drawText('JavaScript');
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
